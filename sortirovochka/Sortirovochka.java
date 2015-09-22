3171022cbd8949c102bf9f98867b9b24304502bd
Konstantin Skobeltsyn <konstantin@skobeltsyn.com>
2015-09-22 13:17:54 +0300

    sort

--- sortirovochka/Sortirovochka.java
+++ sortirovochka/Sortirovochka.java
@@ -1,3 +1,16 @@
 public class Sortirovochka {
-	
+	public static void main(String[] args) {
+		int N = 100;
+		int[] chisla;
+		fillArray(chisla,N);
+	}
+	/**
+	* Этот метод заполняет массив размером N
+	*/
+	static void fillArray(int[] array, int N){
+		array = new int[N];
+		for (int i=0; i<N ;i++ ) {
+			array[i]=i;
+		}
+	}
 }
